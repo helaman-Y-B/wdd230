b37c8d763e35543a8545421460a58aea1f35a118
helaman-Y-B <helamashi@gmail.com>
2021-10-08 22:48:15 -0300

    Preston site

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -1,3 +1,43 @@
 function toggleMenu () {
   document.getElementsByClassName("menu")[0].classList.toggle("responsive");
 };
+
+function date() {
+  const weekDays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ]
+
+  const months = [
+    "Jan", 
+    "Feb", 
+    "Mar", 
+    "Apr", 
+    "May", 
+    "Jun",
+    "Jul", 
+    "Aug", 
+    "Sep", 
+    "Oct", 
+    "Nov", 
+    "Dec"
+  ]
+
+  const currentDate = new Date();
+  const day = currentDate.getUTCDay();
+  const numberDay = currentDate.getDate();
+  const month = currentDate.getMonth();
+  const year = currentDate.getFullYear();
+
+  const monthString = months[month];
+  const dayString = weekDays[day - 1];
+
+  document.querySelector("#current-date").textContent = `${dayString}, ${numberDay} ${monthString} ${year}`;
+};
+
+date();
