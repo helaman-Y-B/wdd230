61e3025a6acae3ab78a7782b37db3855137ad447
helaman-Y-B <helamashi@gmail.com>
2021-10-15 11:26:06 -0300

    Finishing the DOM manipulation activity

--- DOM-practice/js/bom.js
+++ DOM-practice/js/bom.js
@@ -4,22 +4,32 @@ const list = document.querySelector("ul");
 
 let chapterList = []
 
-button.addEventListener("click", function(){
-  if (input === " "){
+button.addEventListener("click", function () {
+
+  let chapter = input.value;
+
+  if (chapter === "") {
     console.log("There is no value");
-    
+
   }
-  else{
-    let chapter = input.value;
+  else {
 
     let item = document.createElement("li");
     let deletButton = document.createElement("button");
 
     item.innerHTML = `<p>${chapter}</p>`;
     deletButton.textContent = "X";
-  
+
 
     list.appendChild(item);
     item.appendChild(deletButton);
+
+    deletButton.addEventListener("click", function () {
+      item.remove();
+      input.focus();
+    })
+    input.value = "";
+    input.focus();
   }
 })
+
