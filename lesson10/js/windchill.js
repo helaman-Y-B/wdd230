36bcb08373f7460597460be79a9292cd9e3bfabf
helaman-Y-B <helamashi@gmail.com>
2021-11-19 11:35:49 -0300

    Added the api for weather info for preston

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,3 +1,47 @@
+function getInput() {
+    const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=da3a67b621a6f2f21a31a6932df16c19";
+    getApi(apiURL);
+};
+
+function getApi(url) {
+    fetch(url)
+        .then((response) => {
+            if (response.ok) {
+                let data = response.json();
+                return data
+            }
+            else {
+                console.log("Response was not ok", response);
+            };
+
+        })
+        .then((data) => {
+            showData(data)
+        });
+};
+
+function showData(data) {
+    let temp = document.querySelector("#avg-temp");
+    let humidity = document.querySelector("#humidity")
+    let windSpeed = document.querySelector("#wind-speed")
+    temp.textContent = data.main.temp;
+    humidity.textContent = `${data.main.humidity}%`;
+    windSpeed.textContent = data.wind.speed;
+
+
+    if (parseInt(data.main.temp) <= 50 && parseInt(data.wind.speed) >= 3) {
+        windChillCalc(data.main.temp, data.wind.speed);
+    } else {
+        document.querySelector("#wind-chill-text").remove("#wind-chill-text");
+    }
+
+    //const img = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    //const desc = data.weather[0].description;
+    //document.getElementById("imagesrc").textContent = img;
+    //document.getElementById("icon").setAttribute("src", img);
+    //document.getElementById("icon").setAttribute("alt", desc);
+}
+
 function windChillCalc(t, s) {
 
     let windChill = 35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16;
@@ -5,11 +49,4 @@ function windChillCalc(t, s) {
     document.querySelector("#wind-chill").textContent = windChill;
 };
 
-const temp = parseInt(document.querySelector("#avg-temp").textContent);
-const windSpeed = parseInt(document.querySelector("#wind-speed").textContent);
-
-if (parseInt(temp) <= 50 && parseInt(windSpeed) >= 3) {
-    windChillCalc(temp, windSpeed);
-} else {
-    document.querySelector("#wind-chill-text").remove("#wind-chill-text");
-}
+getInput()
