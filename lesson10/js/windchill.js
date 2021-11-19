e79237718cc3ba27bd1753255999dab4889223c3
helaman-Y-B <helamashi@gmail.com>
2021-11-19 16:39:47 -0300

    Did the five day fore cast

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,23 +1,42 @@
 function getInput() {
     const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=da3a67b621a6f2f21a31a6932df16c19";
     getApi(apiURL);
+    const foreApiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=508ff6423b7d3397f77fec0caef04f94";
+    getApi(foreApiURL);
 };
 
 function getApi(url) {
-    fetch(url)
-        .then((response) => {
-            if (response.ok) {
-                let data = response.json();
-                return data
-            }
-            else {
-                console.log("Response was not ok", response);
-            };
+    if (url === "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=da3a67b621a6f2f21a31a6932df16c19") {
+        fetch(url)
+            .then((response) => {
+                if (response.ok) {
+                    let data = response.json();
+                    return data
+                }
+                else {
+                    console.log("Response was not ok", response);
+                };
 
-        })
-        .then((data) => {
-            showData(data)
-        });
+            })
+            .then((data) => {
+                showData(data)
+            });
+    } else {
+        fetch(url)
+            .then((response) => {
+                if (response.ok) {
+                    let data = response.json();
+                    return data
+                }
+                else {
+                    console.log("Response was not ok", response);
+                };
+
+            })
+            .then((data) => {
+                showForeCastData(data)
+            });
+    };
 };
 
 function showData(data) {
@@ -33,14 +52,39 @@ function showData(data) {
         windChillCalc(data.main.temp, data.wind.speed);
     } else {
         document.querySelector("#wind-chill-text").remove("#wind-chill-text");
-    }
+    };
 
     //const img = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     //const desc = data.weather[0].description;
     //document.getElementById("imagesrc").textContent = img;
     //document.getElementById("icon").setAttribute("src", img);
     //document.getElementById("icon").setAttribute("alt", desc);
-}
+};
+
+function showForeCastData(data) {
+    let fiveDaysForeCast = [];
+    let temps = document.querySelectorAll(".temp-info");
+    const list = data.list
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].dt_txt.includes("18:00:00")) {
+            fiveDaysForeCast.push(list[i]);
+
+        };
+    };
+
+    fiveDaysForeCast.forEach(cast => {
+        showOutPut(cast, temps)
+    })
+};
+
+function showOutPut(cast, temps) {
+    temps[indexNumber].textContent = `${cast.main.temp}°F`;
+    indexNumber += 1;
+
+    //temps.forEach(temp => {
+    //    temp.textContent = `${cast.main.temp}°F`;
+    //});
+};
 
 function windChillCalc(t, s) {
 
@@ -49,4 +93,6 @@ function windChillCalc(t, s) {
     document.querySelector("#wind-chill").textContent = windChill;
 };
 
-getInput()
+let indexNumber = 0;
+let temp_list = [];
+getInput();
