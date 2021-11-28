a886b6f3e87bd3677ccc0bfe5d8e9482b18339d1
helaman-Y-B <helamashi@gmail.com>
2021-11-27 23:43:25 -0300

    Finished the weather webpage.

---
+++ Final-WeatherMoment/Fish-and-Soda/js/Fish-Haven-js/fish-events.js
@@ -0,0 +1,37 @@
+function input() {
+    const apiURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+    getService(apiURL);
+};
+
+function getService(url) {
+    fetch(url)
+        .then((response) => {
+            if (response.ok) {
+                let data = response.json();
+                return data
+            }
+            else {
+                console.log("Response was not ok", response);
+            };
+        })
+        .then((data) => {
+            placeData(data)
+        });
+};
+
+function placeData(data) {
+    let eventsParagraphs = document.querySelectorAll(".event");
+    let events_list = data["towns"][2].events;
+
+    events_list.forEach(event => {
+        placeOutPut(event, eventsParagraphs);
+    });
+};
+
+function placeOutPut(event, paragraphs) {
+    paragraphs[index].textContent = `${event}`;
+    index += 1;
+};
+
+let index = 0;
+input();
