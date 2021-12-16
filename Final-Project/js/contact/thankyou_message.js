cc9779520b7dea3d5b1b066c3149acb578d6436a
helaman-Y-B <helamashi@gmail.com>
2021-12-15 21:59:53 -0300

    Fixed contact information

--- Final-Project/js/contact/thankyou_message.js
+++ Final-Project/js/contact/thankyou_message.js
@@ -1,3 +1,7 @@
-function actionHandler() {
+function thanksHandler() {
     document.getElementsByClassName("div-elem")[0].classList.toggle("thanksMessage");
 };
+
+let div = document.getElementById("submit-button");
+
+div.addEventListener("click", thanksHandler, false)
